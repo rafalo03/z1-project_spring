6a8e4339fdb4529721d4902bd4755cd80a0c9a1a
Rafał Chorąży <rafal.chorazy@proton.me>
2025-01-23 11:02:36 +0100

    first_commit

---
+++ src/main/java/pl/edu/vistula/z1_project_spring/Z1ProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.z1_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z1ProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z1ProjectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/z1_project_spring/Z1ProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.z1_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z1ProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
